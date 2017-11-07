7267973e49cb778e4361401841a5bbadca3c79fa
asierrodri28 <asier.rodriguez@opendeusto.es>
2017-11-07 09:27:03 +0100

    infor

---
+++ navtreedata.js
@@ -0,0 +1,12 @@
+var NAVTREE =
+[
+  [ "Proyecto", "index.html", ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
